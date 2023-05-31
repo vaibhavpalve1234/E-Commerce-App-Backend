7cdf5bb78919a3933dc777c82224a3dfb0980133
vaibhavpalve1234 <vaibhavpalve1234@gmail.com>
2023-05-31 12:40:23 +0530

    paytm working fine...!!
    Signed-off-by: vaibhavpalve1234 <vaibhavpalve1234@gmail.com>

--- paytmController/payment/config.js
+++ paytmController/payment/config.js
@@ -1,10 +1,12 @@
-require('dotenv').config({path: `config/.env.${process.env.NODE_ENV}`});
+require('dotenv').config({ path: `config/.env.${process.env.NODE_ENV}` });
 module.exports = {
   paytm_config: {
-		MID: process.env.MID,// Provided by Paytm
-		WEBSITE: 'WEBSTAGING',
-    CHANNEL_ID: 'WEB',
-    INDUSTRY_TYPE_ID: 'Retail',
-    PAYTM_MERCHANT_KEY : process.env.PAYTM_MERCHANT_KEY// Provided by Paytm
-	}
+    MID: process.env.MID,// Provided by Paytm
+    WEBSITE: process.env.WEBSITE,
+    CHANNEL_ID: process.env.CHANNEL_ID,
+    INDUSTRY_TYPE_ID: process.env.INDUSTRY_TYPE_ID,
+    PAYTM_MERCHANT_KEY: process.env.PAYTM_MERCHANT_KEY,// Provided by Paytm
+    REQUEST_TYPE: process.env.REQUEST_TYPE,
+    PAYTM_FINAL_URL : process.env.PAYTM_FINAL_URL
+  }
 }
