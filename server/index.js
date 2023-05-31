e2ceba6f7937431fed14fb6c8cb0f16903823341
vaibhavpalve1234 <vaibhavpalve1234@gmail.com>
2023-05-31 12:02:13 +0530

    edited code
    Signed-off-by: vaibhavpalve1234 <vaibhavpalve1234@gmail.com>

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const connectDB = require('../connection/mongodb_connect');
 const bodyParser = require('body-parser');
 const logger = require('../logger');
 const AuthRouter = require('../router/user');
-const ProductRouter = require('../router/product');
 const { notFound, errorHandler } = require('../utils/errorhandler');
 const port = process.env.SERVER_PORT || 3000;
 const app = express();
@@ -15,7 +14,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 
 app.use('/api/v1', AuthRouter)
-app.use('/api/v1', ProductRouter)
 
 app.use(notFound)
 app.use(errorHandler)
