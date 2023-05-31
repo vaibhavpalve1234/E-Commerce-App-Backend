241aa000b4f7b564b8c3b1a8440a4260397740d1
vaibhavpalve1234 <vaibhavpalve1234@gmail.com>
2023-05-31 10:26:13 +0530

    paytm code with payment system
    Signed-off-by: vaibhavpalve1234 <vaibhavpalve1234@gmail.com>

---
+++ router/paytm/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const { payment, generate_checksum , verify_checksum} = require('../../paytmController/paymentController')
+const { authenticateToken } = require('../../utils/jwtToken')
+
+router.get("/payment", payment)
+router.post("/generate_checksum", generate_checksum)
+router.post("/verify_checksum", verify_checksum)
+
+module.exports = router
